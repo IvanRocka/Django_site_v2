47175cbe0b37fe1d75520995eada5d38cf262dbb
IvanRocka <ivan.baukin@mail.ru>
2022-10-22 16:25:27 +0300

    First commit

--- Django_training/cook/blog/models.py
+++ Django_training/cook/blog/models.py
@@ -19,8 +19,8 @@ class Category(MPTTModel):
 
     def __str__(self):
         return self.name
+# sdfdgdgh
 
-#Why i could not commit?!
 class Tag(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
